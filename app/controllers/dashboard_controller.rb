ef389fe573f16da14a50c223979f28bd0b0006ee
notpace <notpace@gmail.com>
2017-10-12 10:51:56 -0400

    Move user initialization to dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,7 @@ class DashboardController < ApplicationController
   # Ensure that the RABBITMQ_URL variable starts with 'amqp://'
   def set_rabbitmq_env
     @rabbitmq_server = (ENV['RABBITMQ_URL'][0, 7] == 'amqp://' ? ENV['RABBITMQ_URL'] : 'amqp://' + ENV['RABBITMQ_URL'])
-    @user = User.find_by(user: ENV['POPLIN_INSPECTOR_NAME'])
+    @user = User.find_or_create_by(user: ENV['POPLIN_INSPECTOR_NAME'])
   end
 
   def index; end
